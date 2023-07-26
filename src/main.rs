3c77b716c8c5c266d4c4daa72e76c3b8e78b28d2
Awais Saeed <awaissaeed530@gmail.com>
2023-07-26 14:43:45 +0500

    user post and get

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,77 @@
+use core::panic;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 use sqlx::{
     sqlite::{SqlitePoolOptions, SqliteQueryResult},
     SqlitePool,
 };
 
-#[derive(sqlx::FromRow, Debug)]
+struct AppState {
+    pool: SqlitePool,
+}
+
+#[derive(sqlx::FromRow, Debug, Serialize, Deserialize)]
 struct User {
+    id: Option<i32>,
     name: String,
     email: String,
 }
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    let pool = SqlitePoolOptions::new().connect("sqlite::memory:").await?;
-    setup(&pool).await?;
-
-    let user: User = User {
-        name: "Awais Saeed".to_owned(),
-        email: "awais@mail.com".to_owned(),
+async fn main() -> Result<(), std::io::Error> {
+    let pool = SqlitePoolOptions::new().connect("sqlite::memory:").await;
+    let pool = match pool {
+        Ok(_pool) => _pool,
+        Err(err) => {
+            panic!("{:?}", err);
+        }
     };
-    save(user, &pool).await?;
-    let users = find_all(&pool).await?;
-    println!("{:?}", users);
-    Ok(())
+
+    match setup(&pool).await {
+        Ok(_) => {}
+        Err(err) => {
+            panic!("{:?}", err);
+        }
+    };
+
+    let state = web::Data::new(AppState { pool });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(save)
+            .service(find_all)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
-async fn save(user: User, pool: &SqlitePool) -> Result<SqliteQueryResult, sqlx::Error> {
-    sqlx::query("INSERT INTO users (name, email) values ($1, $2)")
-        .bind(user.name)
-        .bind(user.email)
-        .execute(pool)
-        .await
+#[post("/user")]
+async fn save(user: web::Json<User>, state: web::Data<AppState>) -> impl Responder {
+    let res = sqlx::query("INSERT INTO users (name, email) values ($1, $2)")
+        .bind(user.name.to_owned())
+        .bind(user.email.to_owned())
+        .execute(&state.pool)
+        .await;
+
+    match res {
+        Ok(_) => HttpResponse::NoContent(),
+        Err(_) => HttpResponse::BadRequest(),
+    }
 }
 
-async fn find_all(pool: &SqlitePool) -> Result<Vec<User>, sqlx::Error> {
-    sqlx::query_as::<_, User>("SELECT * from users")
-        .fetch_all(pool)
-        .await
+#[get("/user")]
+async fn find_all(state: web::Data<AppState>) -> impl Responder {
+    let res = sqlx::query_as::<_, User>("SELECT * from users")
+        .fetch_all(&state.pool)
+        .await;
+
+    match res {
+        Ok(users) => HttpResponse::Ok().json(users),
+        Err(_) => HttpResponse::BadRequest().body(""),
+    }
 }
 
 async fn setup(pool: &SqlitePool) -> Result<SqliteQueryResult, sqlx::Error> {
