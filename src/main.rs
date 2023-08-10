5f7204226ec97ce6e6e3dc3efc9f8eeab11160d1
Awais Saeed <awaissaeed530@gmail.com>
2023-08-10 06:17:52 +0500

    add loggin

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ mod modules;
 
 use core::panic;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, middleware::Logger};
 use modules::user::routes::user_routes;
 use sqlx::{
     sqlite::{SqlitePoolOptions, SqliteQueryResult},
     SqlitePool,
 };
+use env_logger::Env;
 
 struct AppState {
     pool: SqlitePool,
@@ -31,8 +32,13 @@ async fn main() -> Result<(), std::io::Error> {
     };
 
     let state = web::Data::new(AppState { pool });
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    HttpServer::new(move || App::new().app_data(state.clone()).service(user_routes()))
+    HttpServer::new(move || 
+                    App::new()
+                    .wrap(Logger::default())
+                    .wrap(Logger::new("%a %{User-Agent}i"))
+                    .app_data(state.clone()).service(user_routes()))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
